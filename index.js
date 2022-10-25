cd3f09226d56868bb6e77ed8dd9b2c203612a710
Karimuzzaman <pialkarimuzzaman@gmail.com>
2022-10-26 00:27:48 +0600

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('programming master api running');
@@ -8,4 +11,10 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server is running on port:`, port);
+})
+
+const courseCategories = require('./data/courses.json');
+
+app.get('/course-categories', (req, res) => {
+    res.send(courseCategories);
 })
