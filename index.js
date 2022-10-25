43c34c6d12a27b6d265803b196db9c51f54400f4
Karimuzzaman <pialkarimuzzaman@gmail.com>
2022-10-26 00:30:57 +0600

    course api send to react

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ app.listen(port, () => {
     console.log(`Server is running on port:`, port);
 })
 
+const courseCategories = require('./data/courses.json');
+
+app.get('/course-categories', (req, res) => {
+    res.send(courseCategories);
+})
