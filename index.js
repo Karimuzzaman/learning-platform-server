bf517cb1bfdd21ac2bbdad95a70a99958b5c093c
Karimuzzaman <pialkarimuzzaman@gmail.com>
2022-10-26 14:21:55 +0600

    course detail api send

--- index.js
+++ index.js
@@ -18,3 +18,12 @@ const courseCategories = require('./data/courses.json');
 app.get('/course-categories', (req, res) => {
     res.send(courseCategories);
 })
+
+const courses = require('./data/Detail.json');
+
+app.get('/detail/:id', (req, res) => {
+    const id = req.params.id;
+
+    const course_detail = courses.find(course => course.detail_id === id);
+    res.send(course_detail);
+})
