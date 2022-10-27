5c93f4ee946c904191ff4c33047ad30bb80c4122
Karimuzzaman <pialkarimuzzaman@gmail.com>
2022-10-27 18:36:47 +0600

    comment added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+// now, declaring and getting CORS (Cross-Origin Resource Sharing) 
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+//// now, using CORS (Cross-Origin Resource Sharing)
 app.use(cors());
 
 app.get('/', (req, res) => {
@@ -13,14 +15,21 @@ app.listen(port, () => {
     console.log(`Server is running on port:`, port);
 })
 
+// getting course.json api file.
 const courseCategories = require('./data/courses.json');
 
+
+// sending course.json api file.
 app.get('/course-categories', (req, res) => {
     res.send(courseCategories);
 })
 
+
+// getting Detail.json api file.
 const courses = require('./data/Detail.json');
 
+
+// sending Detail.json api file.
 app.get('/detail/:id', (req, res) => {
     const id = req.params.id;
 
